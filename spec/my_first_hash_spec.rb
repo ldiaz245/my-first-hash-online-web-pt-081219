800ce8787330b07deea57c7d9e28b1bee30b5725
LUIS J Alvarez Diaz <ldiaz244@gmail.com>
2019-09-18 22:10:21 +0000

    Automatically backed up by Learn

--- spec/my_first_hash_spec.rb
+++ spec/my_first_hash_spec.rb
@@ -5,8 +5,8 @@ require_relative '../my_first_hash.rb'
 context "Challenge I: Instantiating Hashes" do
   describe "#your_hash" do
     it "uses the literal constructor to create a hash that contains key/value pairs" do 
-      expect(my_hash).to be_a("my_name") = Luis Diaz
-      expect(my_hash.keys.count).to_not eq(0)
+      expect(my_hash).to be_a("my_name") => 78
+      expect(my_hash.keys.count).to_not eq("my_name") => Luis Diaz
     end
   end
 end
